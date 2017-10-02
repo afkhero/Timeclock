ea236fdb6d8aacb24bd14b798aa01475230ca361
Raghava Vemuri <raghava.vemuri@mavs.uta.edu>
2017-10-02 12:19:58 -0500

    Add files via upload

--- emp_time.php
+++ emp_time.php
@@ -85,3 +85,26 @@ function emp_view_times($mysqli, $utaid, $datestr){
 }
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Employee/Admin Clock in/out Page</title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+ 
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+ 
+<link rel="stylesheet" href="style.css" >
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</body>
+</html>
--- timeindex.php
+++ timeindex.php
@@ -1,13 +1,16 @@
 <?php 
 	require_once ($_SERVER['DOCUMENT_ROOT']."/connections/db_connect8.php");
 	include 'emp_time.php'; 
-	include 'admin_time.php'; 
+	//include 'admin_time.php'; 
 	session_start(); 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<meta charset="utf-8"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Employee/Admin Clock in/out Page</title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
+<link rel="stylesheet" href="style.css" type="text/css" />
 
 <style>
 table {border-collapse: collapse;}
@@ -90,6 +93,18 @@ table, th, td{border: 1px solid black;}
 	}
 ?>
 </table>
+
+
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+ 
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+ 
+<link rel="stylesheet" href="style.css" >
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </body>
 
